c5ef1773bec2d8398d41e05c97a288c7bd5df93f
Alef Soares <alefcard.soares@gmail.com>
2022-09-16 07:42:15 -0300

    atualização

--- Exercicios/Exercicio-1.js
+++ Exercicios/Exercicio-1.js
@@ -1,3 +1,9 @@
-// Crie uma função que retorna a string "Olá, " concatenada com um argumento text (a ser passado para a função) e
-// com ponto de exclamação "!" no final.
+/* Crie uma função que retorna a string "Olá, " concatenada com um argumento text (a ser passado para a função) e
+com ponto de exclamação "!" no final.*/
 
+function concatenar(texto) {
+  // return 'Olá, ' + texto + '!'
+  return `Olá, ${texto}!`
+}
+
+concatenar('Mundo')
---
+++ Fundamentos/organizacoao.js
@@ -0,0 +1,11 @@
+console.log('Sentença de codigo')
+
+{
+  {
+    console.log('Olá')
+    console.log('Mundo!')
+  }
+}
+
+/* Javascript é organizado por senteças de codigos, que podem ou nao terminar com ";", 
+e tambem é organizado em blocos de codigos utilizando chaves.*/
